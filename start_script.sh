141a6f4423d5a97199c2d800e9bfbc73573cec7c
Alejandro Caral Ferro <acaral@users.noreply.github.com>
2020-11-27 21:31:55 -0500

    Update start_script.sh

--- start_script.sh
+++ start_script.sh
@@ -5,14 +5,11 @@ sudo apt-get install nginx python3 python3-pip -y
 sudo pip3 install certbot-nginx
 cat > ngrok.smartapphouses.com << EOF
 server {
+        server_name ngrok.smartapphouses.com;
         location / { proxy_pass http://localhost:3333/;}
 }
-server {
-        listen 80 ;
-    server_name ngrok.smartapphouses.com;
-}
 EOF
+
 sudo cp ngrok.smartapphouses.com /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/ngrok.smartapphouses.com /etc/nginx/sites-enabled/
-
 sudo certbot --nginx -d ngrok.smartapphouses.com --non-interactive --agree-tos -m alejandro.caral.ferro@gmail.com

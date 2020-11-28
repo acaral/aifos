a00012debdfa7a7cae08a4048e39c9877645fc1a
Alejandro Caral Ferro <acaral@users.noreply.github.com>
2020-11-28 15:08:56 -0500

    Update start_script.sh

--- start_script.sh
+++ start_script.sh
@@ -1,15 +1,15 @@
-#/bin/bash
+#!/bin/bash
 
 sudo apt-get update
 sudo apt-get install nginx python3 python3-pip -y
 sudo pip3 install certbot-nginx
-cat > aifos.smartapphouses.com << EOF
+cat > ${project}.smartapphouses.com << EOF
 server {
-        server_name aifos.smartapphouses.com;
-        location / { proxy_pass http://localhost:3333/;}
+        server_name ${project}.smartapphouses.com;
+        location / { proxy_pass http://localhost:${port}/;}
 }
 EOF
 
-sudo cp aifos.smartapphouses.com /etc/nginx/sites-available/
-sudo ln -s /etc/nginx/sites-available/aifos.ngrok.smartapphouses.com /etc/nginx/sites-enabled/
-sudo certbot --nginx -d aifos.smartapphouses.com --non-interactive --agree-tos -m alejandro.caral.ferro@gmail.com
+sudo cp ${var.project}.smartapphouses.com /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/${project}.ngrok.smartapphouses.com /etc/nginx/sites-enabled/
+sudo certbot --nginx -d ${project}.smartapphouses.com --non-interactive --agree-tos -m alejandro.caral.ferro@gmail.com
